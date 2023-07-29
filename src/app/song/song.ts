20680420a3762d1d66d0370d21119480fa274f9b
Tristan Meinsma <tristan.meinsma@gmail.com>
2023-07-29 13:44:00 +0200

    seperate artist entity & table view of all songs

--- src/app/song/song.ts
+++ src/app/song/song.ts
@@ -1,7 +1,9 @@
+import { Artist } from "./artist";
+
 export interface Song {
     id: number;
     title: string;
-    artist: string;
+    artists: Artist[];
     imageUrl: string;
     spotifyLink: string;
 }
