d20f01fe13ab10729967d0894ccdf5586f4f4281
Tristan Meinsma <tristan.meinsma@gmail.com>
2023-08-07 09:10:34 +0200

    setup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { SongAdminComponent } from './song/admin/song.admin.component';
 import { SongViewComponent } from './song/view/song.view.component';
@@ -15,7 +16,7 @@ import { SongViewComponent } from './song/view/song.view.component';
     SongViewComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, FormsModule
+    BrowserModule, HttpClientModule, FormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
